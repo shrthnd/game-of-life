0554fc0addd39cee93120e75f693194dc26ecf50
Joe Daddario <joe@shrthnd.org>
2020-11-08 07:38:34 -0500

    Turn on strict-mode; minor refactoring

--- canvas.js
+++ canvas.js
@@ -1,10 +1,28 @@
 const canvas = () => {
+  "use strict"; 
+
+  // utilities 
+  const coord = (x, y) => ( { x, y } )
+  const coinFlip = () => Math.floor(Math.random()*2)
+  const schroedingersBox = () => Math.random() < probabilityOfLife
+  const isDefined = (x) => typeof x !== "undefined"
+  const isUndefined = (x) => typeof x === "undefined"
+  
+  // environment variables 
   const canvas = document.getElementById('canvas')
   const context = canvas.getContext("2d")
-  const space = 12
+  const space = 10
   const gutter = 2 * space
-  const probabilityOfLife = 0.5 
+  const probabilityOfLife = 0.5
+  const brush = {
+    size: coord(space,space),
+    position: false,
+    state: {
+      mousedown: false
+    }
+  }
   
+  // environment state
   let start 
   let paused
   let width
@@ -13,48 +31,23 @@ const canvas = () => {
   let rows
   let grid = []
   
-  const coord = (x, y) => { return { x, y } }
+  // event handlers 
+  const handleMouseDown = () => brush.state.mousedown = true
+  const handleMouseUp = () => brush.state.mousedown = false
+  const handleMouseOver = (e) => brush.position = coord(
+    Math.floor(e.clientX/space)-1, 
+    Math.floor(e.clientY/space)-1
+  )
+  const handleKeypress = () => paused = !paused
 
-  const coinFlip = () => {
-    return Math.floor(Math.random()*2)
-  }
-
-  const schroedingersBox = () => {
-    return Math.random() < probabilityOfLife
-  }
-
-  const brush = {
-    size: coord(space,space),
-    position: false,
-    state: {
-      mousedown: false
-    }
-  }
-
-  const handleMouseDown = () => {
-    brush.state.mousedown = true
-  }
-  
-  const handleMouseUp = () => {
-    brush.state.mousedown = false
-  }
-
-  const handleMouseOver = (e) => {
-    // convert to cell position
-    brush.position = coord(Math.floor(e.clientX/space)-1, Math.floor(e.clientY/space)-1) 
-  }
-  
-  const handleKeypress = () => {
-    paused = !paused
-  }
-
-  const matrix = (rows, columns) => {
-    let matrix = grid.length ? grid.slice(0,rows).map(col => col.slice(0,columns)) : [] // preserve existing elements?
-    for (r=0; r<=rows+1; r++) {
-      for (c=0; c<=columns+1; c++) {
-        if (typeof matrix[r] === "undefined")
+  // initial state of the world
+  const matrix = (rows, columns) => { 
+    let matrix = grid.length ? grid.slice(0,rows).map(col => col.slice(0,columns)) : []
+    for (let r=0; r<=rows+1; r++) {
+      for (let c=0; c<=columns+1; c++) {
+        if (isUndefined(matrix[r]))
           matrix[r] = []
-        if (typeof matrix[r][c] === "undefined")
+        if (isUndefined(matrix[r][c]))
           matrix[r][c] = { 
             state: schroedingersBox(),
             nextState: false
@@ -64,7 +57,7 @@ const canvas = () => {
     return matrix
   }
 
-  const cellState = (r,c) => {
+  const countNeighborCells = (r,c) => {
     const gridLength = grid.length
     const row = grid[r]
     const rowLength = row.length
@@ -108,40 +101,50 @@ const canvas = () => {
     return livingNeighbors
   }
 
-  const updateCell = (currentCell, neighborCells) => {
-    if (!paused && typeof currentCell !== "undefined" ) {
-      // cell is living...
-      if (currentCell.state) {
-        // die by underpopulation
-        neighborCells < 2 ? currentCell.nextState = false : null
+  const updateCell = (currentCell, { r, c }) => {
+    const neighborCells = countNeighborCells(r,c)
 
-        // keep cell alive
-        neighborCells == 2 || neighborCells == 3 ? currentCell.nextState = true : null
-
-        // death by overpopulation
-        neighborCells > 3 ? currentCell.nextState = false : null
-        
-      } else if (neighborCells == 3) {
-        // life!
-        currentCell.nextState = true
+    if (isDefined(currentCell)) {
+      if (!paused) {
+        // cell is living...
+        if (currentCell.state) {
+          // die by underpopulation
+          neighborCells < 2 ? currentCell.nextState = false : null
+  
+          // keep cell alive
+          neighborCells == 2 || neighborCells == 3 ? currentCell.nextState = true : null
+  
+          // death by overpopulation
+          neighborCells > 3 ? currentCell.nextState = false : null
+        } else if (neighborCells == 3) {
+          // life!
+          currentCell.nextState = true
+        }
+      }
+      
+      // highlight living cells
+      if (isDefined(currentCell) && currentCell.state) {
+        neighborCells == 2 ? context.fillStyle = 'violet' : context.fillStyle = 'purple'
+      } else {
+        context.fillStyle = coinFlip() ? '#000' : '#111'
+        // if (neighborCells == 2)
+        //   context.fillStyle = 'green'
+        // if (neighborCells == 3)
+        //   context.fillStyle = 'limegreen'
+        // if (neighborCells == 4)
+        //   context.fillStyle = 'yellow'
+        // if (neighborCells == 5)
+        //   context.fillStyle = 'orange'
+        // if (neighborCells == 6)
+        //   context.fillStyle = 'red'
       }
     }
-    
-    // highlight living cells
-    if (typeof currentCell !== "undefined" && currentCell.state) {
-      neighborCells == 2 ? context.fillStyle = 'violet' : context.fillStyle = 'purple'
-    } else {
-      context.fillStyle = coinFlip() ? '#000' : '#111'
-      // if (neighborCells == 2)
-      //   context.fillStyle = 'green'
-      // if (neighborCells == 3)
-      //   context.fillStyle = 'limegreen'
-    }
 
-    // assign border color (border cells retain state)
+    // set border and toggle on pause (border cells retain state)
     if (!paused && ( r == 0 || r == grid.length-1 || c == 0 || c == grid[0].length-1 )) 
       context.fillStyle = 'lightgreen'  
       
+    // draw the cell
     context.fillRect(c*space, r*space, space, space)
   }
 
@@ -170,38 +173,33 @@ const canvas = () => {
   }
   
   const draw = (timestamp) => {
-    if (typeof start === "undefined") 
+    if (isUndefined(start)) 
       start = timestamp
     
-    if (timestamp-start >= 100/60) {
+    if (timestamp-start >= 10/60) {
       // clear screen
       context.clearRect(0,0, width, height)
       
       // loop through rows in the matrix
-      for (r=0; r<grid.length; r++) {
+      // update cell state
+      for (let r=0; r<grid.length; r++) {
         const row = grid[r]
-
         // loop through columns in row
-        for (c=0; c<row.length; c++) {
+        for (let c=0; c<row.length; c++) {
           const currentCell = row[c]; 
-          const neighborCells = cellState(r,c)
-
           // calculate state of cell in next iteration
-          updateCell(currentCell, neighborCells)
+          updateCell(currentCell, { r, c })
         }
       }
-
-      // update cell state
-      for (r=0; r<grid.length; r++) {
+      // set current state to nextState
+      for (let r=0; r<grid.length; r++) {
         const row = grid[r]
-        for (c=0; c<row.length; c++) {
+        for (let c=0; c<row.length; c++) {
           const currentCell = row[c]; 
           currentCell.state = currentCell.nextState
         }
       }
-
       paintBrush()
-
       start = timestamp
     }
     render()
